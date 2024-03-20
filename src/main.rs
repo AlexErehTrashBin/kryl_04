ab28d1e0a2379f54d72fbe3ea42d6e6f39116b8e
Alexander Ereshkin <xcode111@mail.ru>
2024-03-20 12:47:47 +0300

    Update from trapezoid method to rectangle method. Add remaining term and inaccuracies calculation.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate core;
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
 use std::io::{Error as IOError, stdin, stdout, Write};
-use std::num::{ParseFloatError, ParseIntError};
 use std::process::exit;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
@@ -15,14 +14,6 @@ pub fn get_line<'a>() -> Result<&'a str, IOError> {
     result = String::from(result.trim());
     let result: &'a str = result.leak::<'a>();
     Ok(result)
-}
-
-pub fn parse_to_double(str: &str) -> Result<f64, ParseFloatError> {
-    Ok(f64::from_str(str)?)
-}
-
-pub fn parse_to_uint(str: &str) -> Result<u64, ParseIntError> {
-    Ok(u64::from_str(str)?)
 }
 
 #[derive(Debug, Clone)]
@@ -48,6 +39,12 @@ fn function(x: f64) -> f64 {
     x.atan() / (x.powi(4) + 1.0)
 }
 
+// FIXME это первая производная
+fn second_derivative(x: f64) -> f64 {
+    (1.0/(x.powi(6) + x.powi(4) + x.powi(2) + 1.0)) -
+        (4.0*x.powi(3)*x.atan()) / (x.powi(8) + 2.0*x.powi(4) + 1.0)
+}
+
 fn calculate_accumulated_sum_on_range(
     f: fn(f64) -> f64,
     lower_bound: f64,
@@ -57,16 +54,13 @@ fn calculate_accumulated_sum_on_range(
     let mut local_sum = 0.0;
     let mut i = lower_bound;
     while i + step < upper_bound {
-        let value = f(i);
-        let next_value = f(i + step);
-        let median_value = (value + next_value) / 2.0;
-        local_sum += median_value;
+        local_sum += f(i + step/2.0);
         i += step;
     }
     local_sum
 }
 
-const THREADS_COUNT: i32 = 16;
+const THREADS_COUNT: i32 = 32;
 
 fn calculate_integral_async<'a>(
     f: fn(f64) -> f64,
@@ -108,8 +102,8 @@ fn calculate_integral_async<'a>(
     Ok(result)
 }
 
-const MAX_SAMPLES_COUNT: u64 = 1000000000u64;
-const ASYNC_THRESHOLD_SAMPLES_COUNT: u64 = 10000u64;
+const MAX_SAMPLES_COUNT: u64 = 1_000_000_000u64;
+const ASYNC_THRESHOLD_SAMPLES_COUNT: u64 = 10_000u64;
 
 fn calculate_integral<'a>(
     f: fn(f64) -> f64,
@@ -137,6 +131,22 @@ fn calculate_integral<'a>(
     Ok(result)
 }
 
+fn get_remaining_term(
+    second_derivative: fn(f64) -> f64,
+    lower_bound: f64,
+    upper_bound: f64,
+    step: f64
+) -> f64 {
+    let mut local_max = 0.0;
+    let mut i = lower_bound;
+    while i + step < upper_bound {
+        local_max = second_derivative(i).abs().max(local_max);
+        i += step;
+    }
+    let factor = (upper_bound - lower_bound) * step.powi(2) / 24.0;
+    factor * local_max
+}
+
 
 const EXIT_INCORRECT_LOWER_BOUND: i32 = 1;
 const EXIT_INCORRECT_UPPER_BOUND: i32 = 2;
@@ -146,7 +156,7 @@ const EXIT_UNABLE_TO_CALCULATE: i32 = 4;
 fn main() {
     print!("Введите нижнюю границу: ");
     stdout().flush().unwrap();
-    let lower_bound = parse_to_double(get_line().unwrap())
+    let lower_bound = f64::from_str(get_line().unwrap())
         .inspect_err(|_| {
             eprintln!("Ошибка преобразования ввода в вещественное число");
             exit(EXIT_INCORRECT_LOWER_BOUND);
@@ -154,7 +164,7 @@ fn main() {
         .unwrap();
     print!("Введите верхнюю границу: ");
     stdout().flush().unwrap();
-    let upper_bound = parse_to_double(get_line().unwrap())
+    let upper_bound = f64::from_str(get_line().unwrap())
         .inspect_err(|_| {
             eprintln!("Ошибка преобразования ввода в вещественное число");
             exit(EXIT_INCORRECT_UPPER_BOUND);
@@ -162,17 +172,42 @@ fn main() {
         .unwrap();
     print!("Введите количество отсчётов: ");
     stdout().flush().unwrap();
-    let samples = parse_to_uint(get_line().unwrap())
+    let samples = u64::from_str(get_line().unwrap())
         .inspect_err(|_| {
             eprintln!("Ошибка преобразования ввода в целое число");
             exit(EXIT_INCORRECT_SAMPLES_COUNT);
         })
         .unwrap();
-    let result = calculate_integral(function, lower_bound, upper_bound, samples)
+    let result = calculate_integral(
+        function,
+        lower_bound,
+        upper_bound,
+        samples
+    )
         .inspect_err(|e| {
             eprintln!("{}", e);
             exit(EXIT_UNABLE_TO_CALCULATE);
         })
         .unwrap();
-    println!("Вычисленное значение интеграла: {:}", result);
+    println!("Приближённое значение интеграла: {:.}", result);
+    let result_for_inaccuracy = calculate_integral_async(
+        function,
+        lower_bound,
+        upper_bound,
+        MAX_SAMPLES_COUNT
+    )
+        .inspect_err(|e| {
+            eprintln!("{}", e);
+            exit(EXIT_UNABLE_TO_CALCULATE);
+        })
+        .unwrap();
+    println!("\"Действительное\" значение интеграла: {:.}", result_for_inaccuracy);
+    let absolute_inaccuracy = (result_for_inaccuracy - result).abs();
+    let relative_incaccuracy = absolute_inaccuracy / result;
+    let step = (upper_bound - lower_bound) / samples as f64;
+    println!("Абсолютная погрешность: {:.}", absolute_inaccuracy);
+    let remaining_term_max = get_remaining_term(second_derivative, lower_bound, upper_bound, step);
+    println!("Верхняя граница для Rn: {:.}", remaining_term_max);
+    println!("Абсолютная погрешность соответствует остаточному члену: {:.}", absolute_inaccuracy <= remaining_term_max);
+    println!("Относительная погрешность: {:.}%", relative_incaccuracy * 100.0);
 }
